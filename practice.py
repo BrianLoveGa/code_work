48cefc2f31e5daead0acd353bea5e6db9731fa72
Brian Loveless <Brian.loveless1@yahoo.com>
2019-12-12 16:16:10 -0500

    practice

--- practice.py
+++ practice.py
@@ -15,3 +15,25 @@ string_to_array("Robin Singh")
 string_to_array("CodeWars")
 string_to_array("I love arrays they are my favorite")
 string_to_array("1 2 3")
+
+
+# Create a function that gives a personalized greeting.
+#  This function takes two parameters: name and owner.
+# Use conditionals to return the proper message:
+# case return
+# name equals owner	'Hello boss'
+# otherwise	'Hello guest'
+
+
+def greet(name, owner):
+    if (name.lower() == owner.lower()):
+        # used return for code wars
+        # using print here so can see results
+        print('Hello boss')
+    else:
+        print('Hello guest')
+
+greet("Fred", "Fred")
+greet("Tim", "Fred")
+greet("Sarah", "Fred")
+greet("owner", "OWNER")
