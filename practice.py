ea5490557f2d21f3c45f33bdb4af9e78cef78d39
Brian Loveless <Brian.loveless1@yahoo.com>
2019-12-12 16:44:37 -0500

    iq test

--- practice.py
+++ practice.py
@@ -89,3 +89,43 @@ print(dig_pow(89, 1))
 print(dig_pow(695, 2))
 print(dig_pow(92, 1))
 print(dig_pow(46288, 3))
+
+
+# Bob is preparing to pass IQ test. The most frequent task in this test is to find out which
+# one of the given numbers differs from the others. Bob observed that one number usually differs
+# from the others in evenness. Help Bob — to check his answers, he needs a program that among the
+# given numbers finds one that is different in evenness, and return a position of this number.
+
+# ! Keep in mind that your task is to help Bob solve a real IQ test, which means indexes of the
+# elements start from 1 (not 0)
+
+# ##Examples :
+
+# iq_test("2 4 7 8 10") = > 3 // Third number is odd, while the rest of the numbers are even
+
+# iq_test("1 2 1 1") = > 2 // Second number is even, while the rest of the numbers are odd
+
+def iq_test(numbers):
+    index_odd = None
+    index_even = None
+
+    numbers = numbers.split(' ')
+    count_odd = 0
+    count_even = 0
+
+    for i in range(len(numbers)):
+        if int(numbers[i]) % 2 == 0:
+            count_even += 1
+            index_even = i + 1
+        else:
+            count_odd += 1
+            index_odd = i + 1
+
+    if count_even == 1:
+        print( "index_even")
+    else:
+        print( "index_odd")
+
+
+iq_test("2 4 7 8 10")
+iq_test("1 2 1 1")
