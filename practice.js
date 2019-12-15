f759a1d270558162b6fb836333c1411f13ef444f
Brian Loveless <Brian.loveless1@yahoo.com>
2019-12-14 23:33:13 -0500

    work

--- practice.js
+++ practice.js
@@ -205,3 +205,34 @@ console.log(apple(4.4));
 //////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////
+
+// # Build a function that takes in two arguments(salary, bonus).
+// # Salary will be an integer, and bonus a boolean.
+
+// # If bonus is true, the salary should be multiplied by 10.
+// # If bonus is false, the fatcat did not make enough money and must receive only his stated salary.
+
+// # Return the total figure the individual will receive as a string prefixed with "£" (="\u00A3", JS, Go, and Java),
+// # "$" (C  # , C++, Ruby, Clojure, Elixir, PHP and Python, Haskell, Lua)
+// # or "¥" (Rust).
+
+function bonusTime(salary, bonus){
+    if(bonus === true){
+        let sal = salary*10
+        return "£ " + sal.toString();
+    } else {
+        return "£ " + salary.toString();
+    }
+}
+
+console.log(bonusTime(10000, true))
+console.log(bonusTime(10000, false));
+console.log(bonusTime(10100, true));
+console.log(bonusTime(1023, true));
+
+// works
+
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+
--- practice.py
+++ practice.py
@@ -296,7 +296,6 @@ def well(x):
 # Given an array of integers, return a new array with each value doubled.
 # For example:
 # [1, 2, 3] - -> [2, 4, 6]
-#  print("dangit")
 
 
 # close on this one
