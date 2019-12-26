89a2d2ca51d8af064a0706d1d8d696a25eda4340
Brian Loveless <Brian.loveless1@yahoo.com>
2019-12-26 16:56:56 -0500

    train

--- practice.py
+++ practice.py
@@ -425,3 +425,31 @@ print(is_divisible(12,7,5))
 #######################################
 #######################################
 #######################################
+
+# Write a function named setAlarm which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+
+# The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise. Examples:
+
+# setAlarm(true, true) -> false setAlarm(false, true) -> false setAlarm(false, false) -> false setAlarm(true, false) -> true
+# Test.describe("set_alarm")
+
+# Test.it("returns correct result for all input values")
+# Test.assert_equals(set_alarm(True, True), False,
+#                    "Fails when input is True, True")
+# Test.assert_equals(set_alarm(False, True), False,
+#                    "Fails when input is False, True")
+# Test.assert_equals(set_alarm(False, False), False,
+#                    "Fails when input is False, False")
+# Test.assert_equals(set_alarm(True, False), True,
+#                    "Fails when input is True, False")
+
+def set_alarm(employed, vacation):
+    # Your code here
+    if employed == True and vacation == False:
+        return True
+    else:
+        return False
+# works
+#######################################
+#######################################
+#######################################
